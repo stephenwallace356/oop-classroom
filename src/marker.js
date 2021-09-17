068365d75b22a587c5d0c745b94b72cd836eecf6
Stephen Wallce <stephenwallace356@DESKTOP-7PIDCQF.localdomain>
2021-09-17 13:33:35 -0700

    added maker & student

--- src/marker.js
+++ src/marker.js
@@ -1,5 +1,19 @@
 class Marker {
-  
+constructor(size, color, remaniningInk){
+this.size = size;
+this.color = color;
+this.remaniningInk = remaniningInk;
+    }
+    write(word){
+        const characterCount = word.split('').filter(char=>char!==' ').length
+        
+        this.remaniningInk -= word.length;
+       
+        if(characterCount>this.remaniningInk){
+            return word.slice(0, this.remaniningInk)
+        }
+        return word
+    }
 }
 
 module.exports = Marker
--- src/student.js
+++ src/student.js
@@ -1,5 +1,28 @@
 class Student {
-  
+  constructor(name, skillLevel){
+      this.name = name
+      this.skillLevel = skillLevel
+      this.assignments = []
+  }
+  study(){
+      if (this.skillLevel<100) this.skillLevel +=1
+      return this;
+  }
+attemptAssignment
+
+  doHomework(homework){
+      if(!homework){
+        this.assignments.forEach(assignment => {
+            if(this.skillLevel > assignment.skillLevel) 
+            assignment.completed = true;
+      else assignment.completed = false
+        })
+  }else{
+      if(this.skillLevel > homework.skillLevel) homework.completed = true;
+      else homework.completed = false
+
+      if(homework.skillLevel && homework.skillLevel<=100 && homework.skillLevel>0) this.assignments.push(homework);
+  }
 }
 
 module.exports = Student
