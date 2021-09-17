22c6867284616bd53dcc3a6d97606e8b97128af4
Stephen Wallce <stephenwallace356@DESKTOP-7PIDCQF.localdomain>
2021-09-17 08:07:13 -0700

    built out computer to pass 2nd test

--- src/computer.js
+++ src/computer.js
@@ -1,4 +1,11 @@
 class Computer {
+    constructor(name, operatingSystem, processor, memory, graphics){
+        this.name = name
+        this.operatingSystem =operatingSystem
+        this.processor = processor 
+        this.memory = memory
+        this.graphics = graphics
+    }
   
 }
 
